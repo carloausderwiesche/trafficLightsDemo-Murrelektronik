6e98237c1aab8e164063fa40630ca019685c9857
carloausderwiesche <91966180+carloausderwiesche@users.noreply.github.com>
2024-05-28 07:35:58 +0200

    added start button

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import TrafficLight from "./components/TrafficLight";
+import { Button, Box } from "@mui/material";
+import { yellow } from "@mui/material/colors";
 
 function App() {
+  const [ColorTrafficLight1, setColorTrafficLight1] = useState("green");
+  const [ColorTrafficLight2, setColorTrafficLight2] = useState("red");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen p-10 bg-gray-300">
+      <h1 className="text-center font-bold text-5xl mb-10">Traffic Lights</h1>
+      <Button variant="contained" color="primary" size="large">
+        Start
+      </Button>
+
+      <div className="m-10">
+        <TrafficLight color={ColorTrafficLight1} />
+        <TrafficLight color={ColorTrafficLight2} />
+      </div>
     </div>
   );
 }
