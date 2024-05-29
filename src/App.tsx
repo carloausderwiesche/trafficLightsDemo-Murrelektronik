97be944f38e9c19fd28da59d5fa028f0a2e829c3
carloausderwiesche <91966180+carloausderwiesche@users.noreply.github.com>
2024-05-29 11:29:55 +0200

    added streets

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import TrafficLight from "./components/TrafficLight";
-import { Button } from "@mui/material";
+import { Box, Button, Container } from "@mui/material";
+import AdjustIcon from "@mui/icons-material/Adjust";
 import PedestrianTrafficLight from "./components/PedestrianTrafficLight";
 
 interface LightSequence {
@@ -45,7 +46,10 @@ const App: React.FC = () => {
     setTrafficLightsRunning(true);
 
     if (initialRun) seq = sequence.slice(2);
-    else seq = sequence;
+    else {
+      seq = sequence;
+      console.log(seq);
+    }
 
     let totalDuration = 0;
     seq.forEach(({ color, duration }, index) => {
@@ -72,9 +76,9 @@ const App: React.FC = () => {
 
     const runTrafficLightSequence = () => {
       runLightSequence(lightSequence, setColorTrafficLight1, () => {
+        setInitialRun(false);
         setTimeout(() => {
           runLightSequence(lightSequence, setColorTrafficLight2, () => {
-            setInitialRun(false);
             setTrafficLightsRunning(false);
           });
         }, 1000);
@@ -104,9 +108,33 @@ const App: React.FC = () => {
   }, [isRunning, stopRequested, isPedestrianSequence, trafficLightsRunning]);
 
   return (
-    <div className="w-screen h-screen p-10 bg-gray-300">
-      <h1 className="text-center font-bold text-5xl mb-10">Traffic Lights</h1>
-      <div className="flex justify-center gap-4">
+    <>
+      <Container
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          bgcolor: "lightgray",
+          height: "100vh",
+          width: "100vw",
+        }}
+      >
+        <h1 className="text-center font-bold text-5xl top-10 absolute">
+          Traffic Lights Demo
+        </h1>
+
+        <Button
+          variant="contained"
+          color="primary"
+          size="large"
+          onClick={handleStart}
+          disabled={isRunning}
+          sx={{ margin: "50px" }}
+        >
+          {isRunning ? "Running..." : "Start"}
+        </Button>
+        {/* <div className="flex justify-center gap-4">
         <Button
           variant="contained"
           color="primary"
@@ -116,26 +144,146 @@ const App: React.FC = () => {
         >
           {isRunning ? "Running..." : "Start"}
         </Button>
+      </div> */}
+
+        {/* <div className="m-10 flex gap-10">
+        <TrafficLight color={colorTrafficLight1} />
+        <TrafficLight color={colorTrafficLight2} />
+      </div> */}
+
+        {/* <div className="m-10 grid w-fit place-items-center">
+        <PedestrianTrafficLight color={colorPedestrianLight} />
         <Button
-          variant="contained"
-          color="secondary"
+          color={stopRequested ? "error" : "primary"}
           size="large"
           onClick={handlePedestrianRequest}
           disabled={!isRunning || isPedestrianSequence}
         >
-          Stop for Pedestrian
+          <AdjustIcon />
         </Button>
-      </div>
+      </div> */}
 
-      <div className="m-10 flex gap-10">
-        <TrafficLight color={colorTrafficLight1} />
-        <TrafficLight color={colorTrafficLight2} />
-      </div>
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          width="100%"
+          height="50%"
+          position="relative"
+        >
+          {/* Horizontal Road */}
+          <Box
+            position="absolute"
+            top="50%"
+            left="0"
+            right="0"
+            height="60px"
+            bgcolor="gray"
+            zIndex={1}
+            style={{ transform: "translateY(-50%)" }}
+          />
+          <Box
+            position="absolute"
+            top="50%"
+            left="0"
+            right="0"
+            height="1px"
+            bgcolor="gray"
+            zIndex={2}
+            style={{
+              background:
+                "repeating-linear-gradient(to right, white, white 10px, transparent 10px, transparent 20px)",
+              transform: "translateY(-50%)",
+            }}
+          />
 
-      <div className="m-10 flex justify-center">
-        <PedestrianTrafficLight color={colorPedestrianLight} />
-      </div>
-    </div>
+          {/* Vertical Road */}
+          <Box
+            position="absolute"
+            left="50%"
+            top="0"
+            bottom="0"
+            width="60px"
+            bgcolor="gray"
+            zIndex={1}
+            sx={{ transform: "translateX(-50%)" }}
+          />
+          <Box
+            position="absolute"
+            left="50%"
+            top="0"
+            bottom="0"
+            width="1px"
+            zIndex={2}
+            bgcolor="gray"
+            style={{
+              background:
+                "repeating-linear-gradient(to bottom, white, white 10px, transparent 10px, transparent 20px)",
+              transform: "translateX(-50%)",
+            }}
+          />
+
+          {/* PEDESTRIAL WALK */}
+          <Box
+            position="absolute"
+            top="50%"
+            right="7%"
+            height="60px"
+            width="12%"
+            bgcolor="white"
+            zIndex={3}
+            display="grid"
+            gridRow={3}
+            alignItems="center"
+            justifyItems="center"
+            style={{ transform: "translateY(-50%)" }}
+          >
+            <Box width="98%" height="10px" bgcolor="gray" />
+            <Box width="98%" height="10px" bgcolor="gray" />
+            <Box width="98%" height="10px" bgcolor="gray" />
+          </Box>
+
+          {/* Traffic Lights */}
+
+          <Box
+            position="absolute"
+            top="20%"
+            left="40%"
+            sx={{ transform: "translateX(-50%)" }}
+          >
+            <TrafficLight color={colorTrafficLight1} rotate />
+          </Box>
+          <Box
+            position="absolute"
+            top="75%"
+            left="55%"
+            style={{ transform: "translateY(-50%)" }}
+          >
+            <TrafficLight color={colorTrafficLight2} />
+          </Box>
+          <Box
+            position="absolute"
+            top="25%"
+            right="10%"
+            display="flex"
+            flexDirection="column"
+            alignItems="center"
+            sx={{ transform: "translateY(-50%)" }}
+          >
+            <PedestrianTrafficLight color={colorPedestrianLight} />
+            <Button
+              color={stopRequested ? "error" : "primary"}
+              size="large"
+              onClick={handlePedestrianRequest}
+              disabled={!isRunning || isPedestrianSequence}
+            >
+              <AdjustIcon />
+            </Button>
+          </Box>
+        </Box>
+      </Container>
+    </>
   );
 };
 
